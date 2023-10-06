cb6a1d2d3bc6de5e753a1840d1aabe26fbe3a5ab
zunaxx <127804550+zunaxx@users.noreply.github.com>
2023-10-06 18:46:50 +0600

    Add files via upload

---
+++ practice.sql
@@ -0,0 +1,145 @@
+create table peoples (
+                         UniquelID INT,
+                         first_name VARCHAR(50),
+                         last_name VARCHAR(50),
+                         age INT,
+                         gender VARCHAR(50),
+                         address VARCHAR(50),
+                         email VARCHAR(50),
+                         occupation VARCHAR(50),
+                         nationality VARCHAR(50),
+                         education VARCHAR(50),
+                         languages VARCHAR(50),
+                         isMarried VARCHAR(50),
+                         hasChildren VARCHAR(50),
+                         heigt INT,
+                         weight INT,
+                         phone_numbers VARCHAR(50)
+);
+
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (1, 'Anny', 'Steggles', 64, 'Female', '16th Floor', 'asteggles0@umich.edu', 'Civil Engineer', 'Albania', 'University of Tirana', 'Bengali', true, false, 62, 31, '122-347-5269');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (2, 'Eleanora', 'Mayworth', 12, 'Female', 'Suite 49', 'emayworth1@wikia.com', 'Assistant Media Planner', 'Albania', 'Epoka University', 'Lao', false, true, 64, 48, '243-759-5980');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (3, 'Torre', 'MacKellar', 83, 'Male', 'Room 1850', 'tmackellar2@nasa.gov', 'Help Desk Technician', 'Albania', 'University of Tirana', 'Aymara', false, false, 111, 64, '708-403-2210');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (4, 'Meagan', 'Towl', 43, 'Female', 'Apt 1337', 'mtowl3@webmd.com', 'Web Designer IV', 'Albania', 'Polytechnic University of Tirane', 'Chinese', false, true, 141, 60, '958-406-7619');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (5, 'Maynord', 'Gors', 29, 'Male', 'Suite 37', 'mgors4@bbb.org', 'Statistician I', 'Albania', 'University of Shkodra "Luigj Gurakuqi"', 'Filipino', true, true, 138, 64, '933-431-4068');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (6, 'Guillema', 'Rowlett', 17, 'Female', 'Apt 1206', 'growlett5@yolasite.com', 'Speech Pathologist', 'Albania', 'University of Elbasan "Aleksander Xhuvani"', 'Persian', false, true, 123, 64, '254-630-1843');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (7, 'Lotty', 'Djurevic', 66, 'Female', '4th Floor', 'ldjurevic6@multiply.com', 'Systems Administrator IV', 'Albania', 'Academy of Arts', 'Lithuanian', true, false, 104, 66, '943-833-9536');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (8, 'Donny', 'Tiffin', 2, 'Male', '13th Floor', 'dtiffin7@wsj.com', 'Business Systems Development Analyst', 'Albania', 'University of New York Tirana', 'Bislama', false, true, 160, 65, '408-430-0031');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (9, 'Janek', 'Roskruge', 38, 'Male', '5th Floor', 'jroskruge8@live.com', 'Biostatistician III', 'Albania', 'University of Vlora "Ismail Qemali"', 'Swahili', false, false, 180, 27, '134-602-0144');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (10, 'Dorice', 'Gierck', 58, 'Female', '17th Floor', 'dgierck9@eventbrite.com', 'Office Assistant IV', 'Albania', 'American University of Tirana', 'MДЃori', false, false, 114, 27, '206-825-3350');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (11, 'Lynnette', 'Borg-Bartolo', 60, 'Female', '15th Floor', 'lborgbartoloa@zdnet.com', 'Database Administrator IV', 'Albania', 'Polytechnic University of Tirane', 'Hiri Motu', false, false, 162, 29, '578-469-3738');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (12, 'Cissiee', 'Eskrigge', 20, 'Female', 'PO Box 34448', 'ceskriggeb@indiatimes.com', 'Assistant Media Planner', 'Albania', 'American University of Tirana', 'Quechua', false, true, 144, 34, '545-267-1491');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (13, 'Haywood', 'Spackman', 43, 'Male', '7th Floor', 'hspackmanc@gnu.org', 'Product Engineer', 'Albania', 'University of Shkodra "Luigj Gurakuqi"', 'Haitian Creole', true, false, 177, 40, '167-286-6752');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (14, 'Harald', 'Conan', 75, 'Male', '20th Floor', 'hconand@eventbrite.com', 'Assistant Manager', 'Albania', 'Polytechnic University of Tirane', 'Catalan', false, true, 130, 65, '883-770-5656');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (15, 'Tabbatha', 'Paladini', 7, 'Female', 'Suite 24', 'tpaladinie@odnoklassniki.ru', 'VP Quality Control', 'Albania', 'Agricultural University of Tirane', 'Burmese', true, true, 86, 65, '450-181-9731');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (16, 'Josefa', 'Blacksell', 51, 'Female', '7th Floor', 'jblacksellf@php.net', 'Environmental Tech', 'Albania', 'University of Tirana', 'Catalan', true, true, 138, 42, '926-920-6020');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (17, 'Mace', 'Wabe', 12, 'Male', 'Apt 387', 'mwabeg@e-recht24.de', 'Registered Nurse', 'Albania', 'University of Tirana', 'Telugu', true, false, 119, 25, '451-286-6762');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (18, 'Kenton', 'Tortoishell', 64, 'Male', 'Apt 1797', 'ktortoishellh@epa.gov', 'Recruiting Manager', 'Albania', 'University вЂњPavaresiaвЂќ Vlore', 'MДЃori', true, true, 141, 38, '370-881-2982');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (19, 'Orville', 'Glanester', 43, 'Male', 'PO Box 30114', 'oglanesteri@amazon.de', 'Geological Engineer', 'Albania', 'University of Shkodra "Luigj Gurakuqi"', 'Gujarati', false, false, 160, 65, '703-814-6051');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (20, 'Matt', 'Laxtonne', 51, 'Male', 'Apt 1959', 'mlaxtonnej@sun.com', 'Nurse Practicioner', 'Albania', 'University of Korca "Fan Noli"', 'Punjabi', false, true, 94, 68, '335-854-5600');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (21, 'Vina', 'Wesley', 92, 'Female', 'Apt 1072', 'vwesleyk@ustream.tv', 'Junior Executive', 'Albania', 'Academy of Sports and Physical Training', 'Tsonga', true, true, 103, 37, '114-453-9429');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (22, 'Base', 'Mouton', 77, 'Male', 'Suite 2', 'bmoutonl@diigo.com', 'Food Chemist', 'Albania', 'Epoka University', 'Lao', true, true, 99, 48, '855-768-6035');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (23, 'Francklin', 'Velten', 39, 'Male', 'Room 1939', 'fveltenm@ask.com', 'Editor', 'Albania', 'University of Gjirokstra "Eqerem Cabej"', 'Dutch', false, true, 187, 40, '284-246-4343');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (24, 'Gisella', 'Bramble', 45, 'Polygender', 'PO Box 8433', 'gbramblen@about.com', 'Executive Secretary', 'Albania', 'Polytechnic University of Tirane', 'Moldovan', true, true, 71, 41, '253-451-1866');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (25, 'Murielle', 'Huison', 1, 'Female', 'Suite 18', 'mhuisono@gizmodo.com', 'Senior Developer', 'Albania', 'University of Gjirokstra "Eqerem Cabej"', 'Assamese', true, false, 102, 29, '530-656-5627');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (26, 'Phil', 'Gavagan', 35, 'Male', 'Suite 31', 'pgavaganp@cornell.edu', 'Junior Executive', 'Albania', 'University вЂњPavaresiaвЂќ Vlore', 'Assamese', true, false, 110, 20, '895-672-7308');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (27, 'Harbert', 'Redmain', 75, 'Male', 'Apt 910', 'hredmainq@si.edu', 'Programmer I', 'USA', 'University of New York Tirana', 'Tamil', true, false, 68, 65, '582-470-3736');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (28, 'Tiphanie', 'Jonson', 6, 'Female', '16th Floor', 'tjonsonr@yandex.ru', 'Operator', 'Albania', 'University вЂњPavaresiaвЂќ Vlore', 'Estonian', true, false, 132, 20, '106-290-0077');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (29, 'Tadeas', 'Higbin', 5, 'Male', 'PO Box 79526', 'thigbins@dell.com', 'Help Desk Technician', 'China', 'Academy of Sports and Physical Training', 'Hebrew', false, false, 78, 22, '436-144-4922');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (30, 'Erhart', 'Sivior', 81, 'Agender', 'Suite 23', 'esiviort@yelp.com', 'Compensation Analyst', 'Albania', 'Academy of Arts', 'Tamil', true, true, 145, 39, '351-237-3580');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (31, 'Nancee', 'Jonczyk', 44, 'Female', 'PO Box 40977', 'njonczyku@arizona.edu', 'Structural Engineer', 'Albania', 'Polytechnic University of Tirane', 'Pashto', true, false, 133, 54, '424-535-7246');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (32, 'Livvyy', 'Warry', 80, 'Female', '18th Floor', 'lwarryv@earthlink.net', 'Physical Therapy Assistant', 'Albania', 'University вЂњPavaresiaвЂќ Vlore', 'Estonian', true, true, 105, 31, '936-427-8586');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (34, 'Nealon', 'Feander', 46, 'Male', '4th Floor', 'nfeanderx@xinhuanet.com', 'Data Coordinator', 'Albania', 'Polytechnic University of Tirane', 'Amharic', true, true, 65, 50, '664-651-5021');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (35, 'Willi', 'Moxham', 97, 'Male', 'Suite 26', 'wmoxhamy@meetup.com', 'Accountant II', 'Albania', 'University of Tirana', 'Fijian', true, false, 87, 47, '604-251-2995');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (36, 'Mora', 'Freebury', 17, 'Female', 'Room 411', 'mfreeburyz@mozilla.org', 'Assistant Manager', 'Albania', 'Polytechnic University of Tirane', 'Dhivehi', false, true, 79, 57, '711-762-8749');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (37, 'Nial', 'Sheards', 62, 'Male', 'PO Box 59461', 'nsheards10@smugmug.com', 'Legal Assistant', 'Albania', 'University of Gjirokstra "Eqerem Cabej"', 'Indonesian', false, false, 93, 21, '644-132-7291');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (38, 'Graeme', 'Rosengart', 56, 'Male', '1st Floor', 'grosengart11@answers.com', 'Assistant Manager', 'Albania', 'University of New York Tirana', 'Dari', true, true, 192, 20, '848-792-7304');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (39, 'Mellisent', 'Robertazzi', 32, 'Female', 'PO Box 81415', 'mrobertazzi12@ow.ly', 'Recruiter', 'Albania', 'University of Shkodra "Luigj Gurakuqi"', 'Georgian', true, true, 104, 63, '121-341-4041');
+insert into peoples (UniquelID, first_name, last_name, age, gender, address, email, occupation, nationality, education, languages, isMarried, hasChildren, heigt, weight, phone_numbers) values (40, 'Ryley', 'Duggan', 95, 'Male', '2nd Floor', 'rduggan13@friendfeed.com', 'Senior Cost Accountant', 'Albania', 'University of Shkodra "Luigj Gurakuqi"', 'Oriya', true, false, 76, 52, '646-916-7972');
+
+-- UPDATE people SET address = 'Гражданская 119' WHERE UniquelID = 4 ;
+--
+-- select * from people
+
+update peoples SET address = 'Гражданская 119' WHERE UniquelID =5;
+
+update  people set occupation = 'programmer' where UniquelID= 6;
+
+DELETE FROM peoples WHERE UniquelID = 1;
+
+select UniquelID from peoples
+
+select * from peoples where peoples.nationality like '%China'
+
+SELECT * FROM peoples WHERE occupation LIKE '%Teacher%' AND age BETWEEN 25 AND 35;
+
+SELECT * FROM peoples WHERE isMarried = 'true' AND hasChildren = 'true';
+
+SELECT * FROM peoples WHERE heigt > 170;
+
+SELECT * FROM peoples WHERE gender = 'Male' AND weight < 70;
+
+SELECT AVG(CAST(age AS DECIMAL)) FROM peoples
+
+SELECT occupation, COUNT(*) as count FROM peoples GROUP BY occupation;
+
+SELECT * FROM peoples WHERE age = (SELECT MIN(CAST(age AS INT)) FROM peoples);
+
+SELECT * FROM peoples WHERE email LIKE '%gmail.com%';
+
+SELECT *
+FROM peoples
+ORDER BY CAST(age AS INT) DESC;
+
+select count(*) as count from peoples where address like '% Albania%';
+
+SELECT * FROM peoples WHERE email LIKE 'com%' and phone_numbers LIKE '555%';
+
+select * from peoples where occupation is null;
+
+select * from peoples where isMarried is true
+
+select * from peoples where isMarried = 'false' and hasChildren = 'false'
+
+SELECT * FROM peoples WHERE height BETWEEN 160 AND 170;
+
+SELECT * FROM peoples WHERE languages LIKE '%English%' AND languages LIKE '%Chinese%';
+SELECT * FROM peoples WHERE isMarried = 'false' AND hasChildren = 'true';
+
+SELECT * FROM peoples WHERE weight > 80 OR isMarried = 'true';
+
+SELECT * FROM peoples ORDER BY age ASC, heigt DESC;
+
+SELECT DISTINCT first_name, last_name FROM peoples;
+
+
+SELECT * FROM peoples WHERE nationality IN ('USA', 'Canada');
+
+
+SELECT first_name, address FROM peoples WHERE address LIKE '%119';
+
+SELECT first_name, gender, phone_numbers FROM peoples WHERE phone_numbers LIKE '%55%' OR phone_numbers LIKE '%22%' OR phone_numbers LIKE '%80%';
+
+UPDATE peoples SET occupation = 'Developer' WHERE isMarried = 'false';
+
+UPDATE peoples SET first_name = 'New Name', last_name = 'New Last Name', age = 30, gender = 'Male', address = 'New Address', email = 'newemail@gmail.com', occupation = 'New Occupation', nationality = 'New Nationality', education = 'New Education', languages = 'New Languages', isMarried = 'true', hasChildren = 'true', height = 180, weight = 80, phone_numbers = '555-222-8080' WHERE UniquelID = 2;
+
+UPDATE peoples SET weight = weight + 5 WHERE hasChildren = 'true';
+
+SELECT * FROM peoples WHERE occupation = 'Teacher';
+
+DELETE FROM peoples WHERE age < 20;
+
+SELECT * FROM peoples WHERE address IS NULL OR address = '';
+
+SELECT * FROM peoples ORDER BY heigt DESC LIMIT 3;
+
+SELECT MIN(weight) FROM peoples WHERE isMarried = 'true';
+
+SELECT AVG(age) FROM peoples WHERE isMarried = 'true';
+
+SELECT SUM(weight) FROM peoples WHERE occupation = 'Engineer';
+
+SELECT * FROM peoples WHERE email NOT LIKE '%gmail.com';
+
+SELECT * FROM peoples WHERE occupation = 'Developer' AND age = 40;
